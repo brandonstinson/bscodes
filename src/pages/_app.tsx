fd4ece3fe250f88d0a9bdb5049381af55692ad3f
Brandon Stinson <brandonstinson84@gmail.com>
2022-02-08 00:24:37 -0500

    move animated favicon code into _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Header } from 'components/Header';
-import { AnimatedFavicon } from 'components/AnimatedFavicon';
 import 'global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <title>BS</title>
-    </Head>
-    <Header />
-    <AnimatedFavicon />
-    <Component {...pageProps} />
-  </>
-);
+import favB from '../../public/favicons/faviconB.ico';
+import favS from '../../public/favicons/faviconS.ico';
+import favU from '../../public/favicons/faviconU.ico';
+
+const favicons = [favB, favS, favU];
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <>
+      <Head>
+        <title>BS</title>
+        <link rel="icon" href={favicons[count % favicons.length].src} />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default MyApp;
